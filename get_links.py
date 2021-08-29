4c3c47e7f68ffb40ba38f6618e820328e4af2a35
abder <lahmaimi@gmail.com>
2021-08-29 17:56:19 +0200

    add url extractor with bs4

--- get_links.py
+++ get_links.py
@@ -1,20 +1,36 @@
 """
- get content and hyperlins from a wikipedia page
+ get content and hyperlinks from a wikipedia page
 """
 
 # import package
 import requests
+from bs4 import BeautifulSoup
+import re
 s = requests.Session()
 
 URL = "https://en.wikipedia.org/w/api.php"
 
 PARAMS = {
     "action": "parse",
-    "page": "Pet door",
+    "page": "Cat",
     "format": "json"
 }
 
 r = s.get(url=URL, params=PARAMS)
 data = r.json()
+content = data["parse"]["text"]["*"]
 
-print(data["parse"]["text"]["*"])
+soup = BeautifulSoup(content)
+links = soup.find_all('a', href=True)
+
+links_infos = {}
+wiki_links = []
+
+for link in links:
+    if re.match('/wiki/.*', link['href']):
+        links_infos['link'] = link['href']
+        links_infos['tag'] = link.text
+        wiki_links.append(links_infos)
+        links_infos = {}
+    else:
+        pass
